0eb883214b0b0a3346256bec1dbbb7accc818af1
LoneShadow084 <122130294+LONESHADOW084@users.noreply.github.com>
2023-05-25 09:27:55 -0600

    Update cart.py

--- cart.py
+++ cart.py
@@ -1,95 +1,82 @@
-while True:
-	try:
-		bg = float(input("Enter your budget : "))
+class Item:
+    def __init__(self, name,price):
+        self.name = name
+        self.price = price
+        self.quantity = 0
 
-		s = bg
-	except ValueError:
-		print("PRINT NUMBER AS A AMOUNT")
-		continue
-	else:
-		break
+class ShoppingCart:
+    def __init__(self):
+        self.items = []
 
+    def add_item(self, item, quantity):
+        item_quantity += quantity
+        self.items.append(item)
 
-a = {"name": [], "quant": [], "price": []}
+    def remove_item(self, item):
+        self.ietms.remove(item)
+        item.quantity = 0
 
+    def view_cart(self):
+        if not self.items:
+            print("Your cart is empty.")
+        else:
+            print("Items in your cart:")
+            for item in self.items:
+                total_price += item.price * item.quantity
+                print(f"{item.name} (Quantity: {item.quantity}, Price: ${item.price})")
+            print(f"Total Price: ${total_price}")
 
-b = list(a.values())
+    def checkout(self):
+        if not self.items:
+            print("Your cart is empty. Unable to checkout. Although you can leave not so suspiously.")
+        else:
+            total_price = 0
+            print("Reciept:")
+            for item in self.items:
+                total_price += item.price * item.quantity
+                print(f"{item.name} (Quantity: {item.quantity}, Price: ${item.price})")
+            print(f" Total Price: ${total_price}")
 
+    def shopping_cart_program():
+        cart = ShoppingCart()
+        while True:
+            print("\n--- Walmart ---")
+            print("1. Add item to cart")
+            print("2. Remove item from cart")
+            print("3. View cart")
+            print("4. Checkout")
+            print("5. Run out of store (Quit)")
+            choice = input("Enter your choice:")
 
-na = b[0]
+            if choice == "1":
+                name = input("Enter item name:")
+                price = float(input("Enter item price:"))
+                quantity = int(input("Enter item quantity:"))
+                item = Item(name, price)
+                cart.add_item(item, quantity)
+                print(f"{quantity} {name}(s) added to cart.")
 
+            elif choice == "2":
+                cart.view_cart()
+                if cart.items:
+                    item_name = input("Enter the name of the item to remove:")
+                    for item in cart.items:
+                        if item.name == item_name:
+                            cart.remove_item(name)
+                            print (f"{item.name} removed from the cart. Why don't you want it?")
+                            break
+                    else:
+                        print(f"{item.name} is not in the cart. You probably dropped it back there.")
 
-qu = b[1]
+            elif choice == "3":
+                cart.view_cart()
+                
+            elif choice == "4":
+                cart.checkout()
+                break
 
+            elif choice == "5":
+                print("Hey come back here! Security! We have a runner.")
 
-pr = b[2]
-
-
-while True:
-	try:
-		ch = int(input("1.ADD\n2.EXIT\nEnter your choice : "))
-	except ValueError:
-		print("\nERROR: Choose only digits from the given option")
-		continue
-	else:
-
-		if ch == 1 and s > 0:
-
-			pn = input("Enter product name : ")
-
-			q = input("Enter quantity : ")
-
-			p = float(input("Enter price of the product : "))
-
-			if p > s:
-
-				print("\nCAN, T BUT THE PRODUCT")
-				continue
-
-			else:
-
-				if pn in na:
-
-					ind = na.index(pn)
-
-					qu.remove(qu[ind])
-
-					pr.remove(pr[ind])
-
-					qu.insert(ind, q)
-
-					pr.insert(ind, p)
-
-					s = bg-sum(pr)
-
-					print("\namount left", s)
-				else:
-
-					na.append(pn)
-
-					qu.append(q)
-
-					pr.append(p)
-
-					s = bg-sum(pr)
-
-					print("\namount left", s)
-
-		elif s <= 0:
-			print("\nNO BUDGET")
-		else:
-			break
-
-
-print("\nAmount left : Rs.", s)
-
-
-if s in pr:
-
-	print("\nAmount left can buy you a", na[pr.index(s)])
-
-print("\n\n\nGROCERY LIST")
-
-
-for i in range(len(na)):
-	print(na[i], qu[i], pr[i])
+            else:
+                print("Invalid. Please try again.")
